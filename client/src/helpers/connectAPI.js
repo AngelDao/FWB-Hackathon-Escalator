63e70cc5e26ce0043d7ebb31c63a0eca8b5e7476
DanWizard <tortortorented@gmail.com>
2021-08-29 17:27:20 -0500

    add rinkeby to api

--- client/src/helpers/connectAPI.js
+++ client/src/helpers/connectAPI.js
@@ -1,7 +1,7 @@
 import api from "etherscan-api";
 
 export const getAPI = async () => {
-  const etherscan = api.init(process.env.REACT_APP_ETHERSCAN);
+  const etherscan = api.init(process.env.REACT_APP_ETHERSCAN, "rinkeby");
   console.log(etherscan);
   return { etherscan };
 };
